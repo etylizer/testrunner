74023b3d928a6bcb17abef0cc4327a9f5b7f8ab8
Albert Schimpf <git@schimpf.link>
2024-06-05 17:53:13 +0200

    Missed some remote calls, removed tests

--- eqwalizer-src/pass_list/list_25.erl
+++
@@ -1,8 +0,0 @@
--module(list_25).
-%%% Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
-%%% This source code is licensed under the Apache 2.0 license found in 
-%%% the LICENSE file in the root directory of this source tree.
--compile([export_all, nowarn_export_all]).
--spec head([A]) -> maybe:maybe(A).
-head([H | _]) -> {'$#maybe:maybe.just', H};
-head([]) -> {'$#maybe:maybe.nothing'}.
--- eqwalizer-src/pass_list/list_26.erl
+++
@@ -1,8 +0,0 @@
--module(list_26).
-%%% Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
-%%% This source code is licensed under the Apache 2.0 license found in 
-%%% the LICENSE file in the root directory of this source tree.
--compile([export_all, nowarn_export_all]).
--spec tail([A]) -> maybe:maybe([A]).
-tail([_ | T]) -> {'$#maybe:maybe.just', T};
-tail([]) -> {'$#maybe:maybe.nothing'}.
